c37414509a99ac8ac0f4bc690d9002e3f9de1ed7
Ben <bodoom08@gmail.com>
2020-08-06 05:55:22 +0100

    Fix auth in bottom

--- application/views/common/layout/bottom.php
+++ application/views/common/layout/bottom.php
@@ -27,9 +27,7 @@
                     </ul>
                 </div>
             </div>
-            <input type="hidden" class="loggedId" value="<?php if (isset($_SESSION['id'])) {
-                                                                e($_SESSION['id']);
-                                                            } ?>">
+            <input type="hidden" class="loggedId" value="<?php echo e($_SESSION['id']); ?>">
 
             <div class="col-lg-4 col-12">
                 <div class="foot footer-email">
