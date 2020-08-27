50cb473bb676d0d7a293cf5cc4db70826d5dd9cc
Ben <bodoom08@gmail.com>
2020-08-27 14:40:27 +0100

    Test telnyx messaing profile

--- application/helpers/telnyx_number_helper.php
+++ application/helpers/telnyx_number_helper.php
@@ -79,6 +79,72 @@ function myNumbersHelper($number)
 	return _outputjson($response);
 }
 
+
+function assign_messaging_profile($number)
+{
+	ini_set('display_errors', 1);
+	$CI = &get_instance();
+
+	$params = [
+		'messaging_profile_id' =>  MESSAGE_PROFILE_ID,
+		// "connection_id" => TEXML_APP_ID,
+	];
+
+	$curl = curl_init();
+
+	// curl -X PATCH \
+	// --header "Content-Type: application/json" \
+	// --header "Accept: application/json" \
+	// --header "Authorization: Bearer YOUR_API_KEY" \
+	// --data '{"messaging_profile_id": "e51f3bde-l4b7-464d-bc90-47196f9dd36b"}' \
+	// https://api.telnyx.com/v2/phone_numbers/{id}/messaging
+
+
+	curl_setopt($curl, CURLOPT_URL, "https://api.telnyx.com/v2/phone_numbers/" . $number . "/messaging");
+	curl_setopt($curl, CURLOPT_HTTPHEADER, [
+		"Accept: application/json",
+		"Cache-Control: no-cache",
+		"Connection: keep-alive",
+		"Content-Type: application/json",
+		"cache-control: no-cache",
+		"Authorization: Bearer " . TELNYX_API_KEY
+	]);
+
+
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
+	curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($params));
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+
+
+	// $curl = curl_init();
+
+	// curl_setopt_array($curl, array(
+	// 	CURLOPT_URL => "https://api.telnyx.com/v2/phone_numbers/" . $number,
+	// 	CURLOPT_RETURNTRANSFER => true,
+	// 	CURLOPT_ENCODING => "",
+	// 	CURLOPT_MAXREDIRS => 10,
+	// 	CURLOPT_TIMEOUT => 0,
+	// 	CURLOPT_FOLLOWLOCATION => true,
+	// 	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	// 	CURLOPT_CUSTOMREQUEST => "PATCH",
+	// 	CURLOPT_POSTFIELDS => json_encode($params),
+	// 	CURLOPT_HTTPHEADER => array(
+	// 		"Accept: application/json",
+	// 		"Cache-Control: no-cache",
+	// 		"Connection: keep-alive",
+	// 		"Content-Type: application/json",
+	// 		"cache-control: no-cache",
+	// 		"Authorization: Bearer " . TELNYX_API_KEY
+	// 	),
+	// ));
+
+	$response = curl_exec($curl);
+
+	curl_close($curl);
+	return _outputjson($response);
+}
+
 function _outputjson($json)
 {
 	return json_decode($json, true);
