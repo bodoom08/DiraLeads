99838dab804674bc1ba3e8a69205ef6a6c0fdc28
Ben <bodoom08@gmail.com>
2020-08-31 04:42:51 +0100

    Add create and update date on my-property cards

--- application/views/my-properties.php
+++ application/views/my-properties.php
@@ -62,6 +62,9 @@ $this->load->view('common/front_end_layout/top', [
                                                                         <h4 class="<?php echo ($value['status'] == 'inactive' ? 'resume_prop' : ''); ?>">For <?php echo ucfirst($value['for']); ?></h4>
                                                                         <p><span class="flaticon-placeholder <?php echo ($value['status'] == 'inactive' ? 'resume_prop' : ''); ?>"><i class="fa fa-map-marker" aria-hidden="true"></i></span> <?php echo $value['street'] ?></p>
                                                                         <!-- <p><span class="flaticon-placeholder <?php echo ($value['status'] == 'inactive' ? 'resume_prop' : ''); ?>"><i class="fa fa-phone" aria-hidden="true"></i></span> <?php echo $value['number'] ?></p> -->
+                                                                        <ul class="prop_details <?php echo ($value['status'] == 'inactive' ? 'resume_prop' : ''); ?>">
+                                                                            <li class="list-inline-item"><a href="#"><span> <i class="fa fa-phone" aria-hidden="true"></i></span><?php echo $value['number']; ?></a></li>
+                                                                        </ul>
                                                                         <?php if ($value['is_annual'] == 'true') { ?>
                                                                             <ul class="prop_details <?php echo ($value['status'] == 'inactive' ? 'resume_prop' : ''); ?>">
 
@@ -70,9 +73,10 @@ $this->load->view('common/front_end_layout/top', [
                                                                             </ul>
                                                                         <?php } ?>
                                                                         <ul class="prop_details <?php echo ($value['status'] == 'inactive' ? 'resume_prop' : ''); ?>">
-
-                                                                            <li class="list-inline-item"><a href="#"><span> <i class="fa fa-phone" aria-hidden="true"></i></span><?php echo $value['number']; ?></a></li>
-
+                                                                            <li class="list-inline-item"><a href="#"><span> <i class="fa fa-calendar" aria-hidden="true"></i></span>Created: <?php echo $value['created_at']; ?></a></li>
+                                                                        </ul>
+                                                                        <ul class="prop_details <?php echo ($value['status'] == 'inactive' ? 'resume_prop' : ''); ?>">
+                                                                            <li class="list-inline-item"><a href="#"><span> <i class="fa fa-calendar" aria-hidden="true"></i></span>Updated: <?php echo $value['updated_at']; ?></a></li>
                                                                         </ul>
                                                                         <?php if ($value['sold'] != 'true') { ?>
                                                                             <ul class="action-sec">
