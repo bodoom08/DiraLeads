c2693b3cb5c2f2858f208b4b26b744e2747ce46d
Ben <bodoom08@gmail.com>
2020-09-13 16:06:12 +0100

    Remove price on rental card on map

--- application/views/properties.php
+++ application/views/properties.php
@@ -981,7 +981,7 @@
 
 
             document.getElementById('property-overview-image').src = `/uploads/${image}`;
-            document.getElementById('property-overview-price').innerHTML = `$${days_price ? days_price : 0}/dy, $${weekly_price ? weekly_price : 0}/wk`;
+            // document.getElementById('property-overview-price').innerHTML = `$${days_price ? days_price : 0}/dy, $${weekly_price ? weekly_price : 0}/wk`;
             document.getElementById('property-overview-capacity').innerHTML = `<span><svg class="svg" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M9.196 14.603h15.523v.027h1.995v10.64h-3.99v-4.017H9.196v4.017h-3.99V6.65h3.99v7.953zm2.109-1.968v-2.66h4.655v2.66h-4.655z" fill="#869099"></path></svg>${bedrooms} bd</span><span><svg class="svg" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M23.981 15.947H26.6v1.33a9.31 9.31 0 0 1-9.31 9.31h-2.66a9.31 9.31 0 0 1-9.31-9.31v-1.33h16.001V9.995a2.015 2.015 0 0 0-2.016-2.015h-.67c-.61 0-1.126.407-1.29.965a2.698 2.698 0 0 1 1.356 2.342H13.3a2.7 2.7 0 0 1 1.347-2.337 4.006 4.006 0 0 1 3.989-3.63h.67a4.675 4.675 0 0 1 4.675 4.675v5.952z" fill="#869099"></path></svg>${bathrooms} ba</span>`;
             document.getElementById('property-overview-address').innerHTML = `${title}`;
             document.getElementById('property-overview-city').innerHTML = `${street}`;
