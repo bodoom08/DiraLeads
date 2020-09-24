21497065d3eccad07a58fb38229ff890401eed4d
muhammad-solution <muhhammad.solution@outlook.com>
2020-09-24 11:17:12 -0400

    Remove job queue

--- application/libraries/JobQueue.php
+++ application/libraries/JobQueue.php
@@ -11,9 +11,9 @@ class JobQueue
     {
         $this->CI = &get_instance();
         $this->CI->load->model('M_property');
-        $this->CI->load->model('M_users');
+        // $this->CI->load->model('M_users');
 
-        \Telnyx\Telnyx::setApiKey(TELNYX_API_KEY);
+        // \Telnyx\Telnyx::setApiKey(TELNYX_API_KEY);
 
         log_message('info', 'starting worker....');
     }
@@ -28,19 +28,19 @@ class JobQueue
         try {
             foreach ($jobs as $job) {
                 echo ($job['property_id'] . ' - ' . $job['subscriber_id']);
-                $subscriber = $this->db->select('country_code, mobile')
-                    ->from('users')
-                    ->where('id', $job['user_id'])
-                    ->get()->row();
-                $number = $subscriber['country_code'] . $subscriber['mobile'];
+                // $subscriber = $this->db->select('country_code, mobile')
+                //     ->from('users')
+                //     ->where('id', $job['user_id'])
+                //     ->get()->row();
+                // $number = $subscriber['country_code'] . $subscriber['mobile'];
 
-                \Telnyx\Call::Create([
-                    "from" => "+15166361518", // Your Telnyx number
-                    "to" => $number,
-                    [
-                        'url' => base_url() . 'webhook/subscriber_receive',
-                    ]
-                ]);
+                // \Telnyx\Call::Create([
+                //     "from" => "+15166361518", // Your Telnyx number
+                //     "to" => $number,
+                //     [
+                //         'url' => base_url() . 'webhook/subscriber_receive',
+                //     ]
+                // ]);
 
 
                 $this->CI->M_property->deleteJob($job['id']);
